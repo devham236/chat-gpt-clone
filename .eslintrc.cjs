f054f73ad85a6faf5eb946f74c798b5f87bf62bf
devham236 <90037750+devham236@users.noreply.github.com>
2023-11-29 13:35:38 +0100

    added prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,12 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "prettier"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
@@ -26,4 +27,4 @@ module.exports = {
     "react/prop-types": 0,
     "no-undef": 0,
   },
-};
+}
