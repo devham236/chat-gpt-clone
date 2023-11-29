2d87373707b642aaff31de1fb2aa99401115a8b3
devham236 <90037750+devham236@users.noreply.github.com>
2023-11-29 14:39:45 +0100

    added fontawesome icons and google fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react"
 
 const App = () => {
-  return <div className="bg-red-300">App</div>
+  return (
+    <div className="">
+      <section className="side-bar">
+        <div className="top-part">
+          <div>
+            {/*OpenAI-Icon*/}
+            {/*Title*/}
+          </div>
+          {/*New-chat-Icon*/}
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default App
