cf47db35def86a8a8b346e753151ffd3ad91317d
devham236 <90037750+devham236@users.noreply.github.com>
2023-12-05 09:24:04 +0100

    sidebar opening/closing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 
 const App = () => {
+  const [sidebarOpened, setSidebarOpened] = useState(true)
   return (
     <div className="w-[100vw] h-[100vh] bg-[#343541]">
-      <section className="side-bar w-[20%] h-full bg-black relative">
+      <section
+        className={`side-bar ${
+          sidebarOpened ? "w-[20%]" : "w-0"
+        } h-full bg-black relative`}
+      >
         <div className="top-part">
           <div>
             {/*OpenAI-Icon*/}
@@ -12,8 +17,15 @@ const App = () => {
           {/*New-chat-Icon*/}
         </div>
         <ul></ul>
-        <div className="absolute right-[-20px] top-[50%] cursor-pointer">
-          <i className="fa-solid fa-angle-right text-lg text-[#8e8ea0]"></i>
+        <div
+          onClick={() => setSidebarOpened(!sidebarOpened)}
+          className="absolute right-[-20px] top-[50%] cursor-pointer"
+        >
+          <i
+            className={`fa-solid fa-angle-${
+              sidebarOpened ? "left" : "right"
+            } text-lg text-[#8e8ea0]`}
+          ></i>
         </div>
       </section>
     </div>
