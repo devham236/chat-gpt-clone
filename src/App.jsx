03aac365436aabd6b23d195c364c699c7aa905cc
devham236 <90037750+devham236@users.noreply.github.com>
2023-12-05 10:25:37 +0100

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,44 @@ import React, { useState } from "react"
 
 const App = () => {
   const [sidebarOpened, setSidebarOpened] = useState(true)
+
+  const array = new Array(5).fill("item")
+
   return (
-    <div className="w-[100vw] h-[100vh] bg-[#343541]">
+    <div className="w-[100vw] h-[100vh] bg-[#343541] flex">
       <section
         className={`side-bar ${
-          sidebarOpened ? "w-[20%]" : "w-0"
-        } h-full bg-black relative`}
+          sidebarOpened ? "w-[350px] p-2" : "w-0 p-0"
+        } h-full bg-black relative text-white flex flex-col`}
       >
-        <div className="top-part">
-          <div>
-            {/*OpenAI-Icon*/}
-            {/*Title*/}
+        <div
+          className={`top-part ${
+            sidebarOpened ? "p-3" : "hidden p-0"
+          } flex items-center justify-between`}
+        >
+          <div className="flex items-center justify-center">
+            <i className="fa-brands fa-react text-3xl mr-1"></i>
+            <h1 className="ml-1 text-xl font-bold">New Chat</h1>
           </div>
-          {/*New-chat-Icon*/}
+          <div className="cursor-pointer opacity-50 hover:opacity-100 duration-300">
+            <i className="fa-regular fa-square-plus text-3xl"></i>
+          </div>
         </div>
-        <ul></ul>
+
+        <ul
+          className={`w-full max-h-full overflow-auto flex flex-col mt-4 ${
+            sidebarOpened ? "" : "hidden"
+          }`}
+        >
+          {array.map((item, index) => (
+            <li
+              key={index}
+              className="w-full hover:bg-[#656570] duration-300 p-3 mb-1 last:mb-0 rounded-md cursor-pointer"
+            >
+              Test
+            </li>
+          ))}
+        </ul>
         <div
           onClick={() => setSidebarOpened(!sidebarOpened)}
           className="absolute right-[-20px] top-[50%] cursor-pointer"
@@ -24,10 +47,12 @@ const App = () => {
           <i
             className={`fa-solid fa-angle-${
               sidebarOpened ? "left" : "right"
-            } text-lg text-[#8e8ea0]`}
+            } text-lg text-[#ececf1] opacity-50 hover:opacity-100 duration-300`}
           ></i>
         </div>
       </section>
+
+      <section className="chat-div w-full h-full p-3"></section>
     </div>
   )
 }
