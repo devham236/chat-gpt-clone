a2d938874d3640b21e93744895f1efe1d289e8c4
devham236 <90037750+devham236@users.noreply.github.com>
2023-12-05 09:15:14 +0100

    sidebar skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react"
 
 const App = () => {
   return (
-    <div className="">
-      <section className="side-bar">
+    <div className="w-[100vw] h-[100vh] bg-[#343541]">
+      <section className="side-bar w-[20%] h-full bg-black relative">
         <div className="top-part">
           <div>
             {/*OpenAI-Icon*/}
@@ -11,8 +11,10 @@ const App = () => {
           </div>
           {/*New-chat-Icon*/}
         </div>
-
         <ul></ul>
+        <div className="absolute right-[-20px] top-[50%] cursor-pointer">
+          <i className="fa-solid fa-angle-right text-lg text-[#8e8ea0]"></i>
+        </div>
       </section>
     </div>
   )
