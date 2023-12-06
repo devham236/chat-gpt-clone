594279c8ac077ea560fef27eec808d7f880c71b2
devham236 <90037750+devham236@users.noreply.github.com>
2023-12-06 14:10:58 +0100

    first route

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,32 @@ const server = express()
 server.use(express.json())
 server.use(cors())
 
+server.post("/completions", async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: "how are you?" }],
+      max_tokens: 100,
+    }),
+  }
+
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    )
+    const data = await response.json()
+    res.status(200).send(data)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
 server.listen(8000, () => {
   console.log("Your server is running on port: 8000")
 })
